85885ca55fb3c28cf55aa19caa3bedfe62f560d3
Benjamin ADOLPHE <adolphe906@gmail.com>
2021-10-11 08:26:00 +0200

    removed unused model

--- lib/Requests/http_request.dart
+++ lib/Requests/http_request.dart
@@ -4,23 +4,8 @@ import 'package:http/http.dart';
 import 'package:meteo/Constants/constants.dart';
 import 'package:meteo/Models/current_weather_model.dart';
 import 'package:meteo/Models/seven_days_forecast_model.dart';
-import 'package:meteo/Models/weather_model.dart';
 
 class Network {
-  Future<WeatherModel> getWeatherFromCityName(
-      {required String cityName}) async {
-    var url =
-        "https://api.openweathermap.org/data/2.5/forecast?q=$cityName,fr&mode=json&appid=${Constant.api_Key}";
-    final response = await get(Uri.parse(url));
-    print("URL : ${Uri.encodeFull(url)}");
-
-    if (response.statusCode == 200) {
-      print("weather Data : ${response.body}");
-      return WeatherModel.fromJson(json.decode(response.body));
-    } else {
-      throw Exception("Erreur en essayant d'obtenir la météo");
-    }
-  }
 
   Future<CurrentWeatherModel> getCurrentWeatherFromCityName(
       {required String cityName}) async {
--- lib/cubit/city_name_cubit.dart
+++ lib/cubit/city_name_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
-import 'package:meteo/Models/weather_model.dart';
 import 'package:meteo/Requests/http_request.dart';
 
 class CityNameCubit extends Cubit<String> {
